0b01ced13d522f4eb84ff88f0aabb6fbba44ce2d
Rein van der Woerd <reinvanderwoerd@me.com>
2024-06-06 16:05:57 +0200

    Reloading working!

--- src/NDSProvider.ts
+++ src/NDSProvider.ts
@@ -1,49 +1,52 @@
 import * as vscode from 'vscode';
 import { NDSPreview } from './NDSPreview';
 
-export class PdfCustomProvider implements vscode.CustomReadonlyEditorProvider {
-  public static readonly viewType = 'pdf.preview';
+export class NDSCustomProvider implements vscode.CustomReadonlyEditorProvider {
+    public static readonly viewType = 'nds-emulatorjs-vscode.preview';
 
-  private readonly _previews = new Set<NDSPreview>();
-  private _activePreview: NDSPreview | undefined;
+    private readonly _previews = new Set<NDSPreview>();
+    private _activePreview: NDSPreview | undefined;
 
-  constructor(private readonly extensionRoot: vscode.Uri) {}
+    constructor(private readonly extensionRoot: vscode.Uri) { }
 
-  public openCustomDocument(uri: vscode.Uri): vscode.CustomDocument {
-    return { uri, dispose: (): void => {} };
-  }
+    public openCustomDocument(uri: vscode.Uri): vscode.CustomDocument {
+        return { uri, dispose: (): void => { } };
+    }
 
-  public async resolveCustomEditor(
-    document: vscode.CustomDocument,
-    webviewEditor: vscode.WebviewPanel
-  ): Promise<void> {
-    const preview = new NDSPreview(
-      this.extensionRoot,
-      document.uri,
-      webviewEditor
-    );
-    this._previews.add(preview);
-    this.setActivePreview(preview);
+    public async resolveCustomEditor(
+        document: vscode.CustomDocument,
+        webviewEditor: vscode.WebviewPanel
+    ): Promise<void> {
+        const preview = new NDSPreview(
+            this.extensionRoot,
+            document.uri,
+            webviewEditor
+        );
 
-    webviewEditor.onDidDispose(() => {
-      preview.dispose();
-      this._previews.delete(preview);
-    });
 
-    webviewEditor.onDidChangeViewState(() => {
-      if (webviewEditor.active) {
+
+        this._previews.add(preview);
         this.setActivePreview(preview);
-      } else if (this._activePreview === preview && !webviewEditor.active) {
-        this.setActivePreview(undefined);
-      }
-    });
-  }
 
-  public get activePreview(): NDSPreview | undefined {
-    return this._activePreview;
-  }
+        webviewEditor.onDidDispose(() => {
+            preview.dispose();
+            this._previews.delete(preview);
+        });
 
-  private setActivePreview(value: NDSPreview | undefined): void {
-    this._activePreview = value;
-  }
+        webviewEditor.onDidChangeViewState(() => {
+            if (webviewEditor.active) {
+                this.setActivePreview(preview);
+            } else if (this._activePreview === preview && !webviewEditor.active) {
+                this.setActivePreview(undefined);
+            }
+        });
+    }
+
+    public get activePreview(): NDSPreview | undefined {
+        return this._activePreview;
+    }
+
+    private setActivePreview(value: NDSPreview | undefined): void {
+        this._activePreview = value;
+    }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,87 +1,30 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { NDSCustomProvider } from './NDSProvider';
 
 
-const webviewHtml = `<!DOCTYPE html>
-	<html>
-	<head>
-		<title>NDS Embed</title>
-	</head>
 
-	<body>
-		<input type="file" id="load"></input>
-		<button id="reload">Reload</button>
-
-		<desmond-player id="player"></desmond-player>
-		
-		<script src="https://cdn.jsdelivr.net/gh/Unzor/desmond/cdn/desmond.min.js"></script>
-
-		<script>
-			let btn = document.getElementById('load');
-			let reload = document.getElementById('reload');
-
-			btn.onchange = () => {
-				let nds = URL.createObjectURL(btn.files[0]);
-				document.getElementById("player").loadURL(nds);
-			}
-
-			reload.onclick = () => {
-				window.location.reload();
-			}
-
-			var host = document.querySelector('desmond-player')
-			var style = document.createElement('style')
-			style.innerHTML = '#player { display: flex; flex-direction: column; max-width: 600px; width: 100%; grid-gap: 10px}'
-			host.shadowRoot.appendChild(style)
-		</script>
-
-		<style>
-			body {
-			/* background-color: grey;*/
-			}
-			desmond-player {
-				image-rendering: pixelated;
-				width: 100%;
-				display: flex;
-				align-items: center;
-				flex-direction: column;
-			}
-		</style>
-	</body>
-
-	</html>
-`;
-
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext): void {
+	const extensionRoot = vscode.Uri.file(context.extensionPath);
+	// Register our custom editor provider
+	const provider = new NDSCustomProvider(extensionRoot);
 	context.subscriptions.push(
-		vscode.commands.registerCommand('nds-emulatorjs-vscode.openNDSView', () => {
-		  // Create and show panel
-		  const panel = vscode.window.createWebviewPanel(
-			'nds-emulatorjs-vscode',
-			'NDS Emulator',
-			vscode.ViewColumn.Two,
+		vscode.window.registerCustomEditorProvider(
+			NDSCustomProvider.viewType,
+			provider,
 			{
-				enableScripts: true
+				webviewOptions: {
+					enableFindWidget: false, // default
+					retainContextWhenHidden: true,
+				},
 			}
-		  );
-
-		  
-	
-		  // And set its HTML content
-		  panel.webview.html = webviewHtml;
-		})
-	  );
-	}
-
-
-
+		)
+	);
+}
 
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
 
 
